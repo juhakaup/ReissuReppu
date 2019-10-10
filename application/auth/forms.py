24b43c10167061862fbe9f0b58518c02bc884211
Juha KauppinenD <juha.kauppinen@helsinki.fi>
2019-10-10 17:37:41 +0300

    Käyttäjän syöttämän validointi toisen kentän avulla

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -11,6 +11,7 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     email = StringField("Email", [validators.Length(min=2, max=100, message=("Email must be between 4 and 100 characters long.")), validators.Email()])
     password = PasswordField("Password", [validators.Length(min=4, max=100, message=("Password must be between 4 and 100 characters long.")), validators.DataRequired()])
+    confirm = PasswordField("Confirm password", [validators.EqualTo("password", message="Passwords don't match")])
     name = StringField("Name", [validators.Length(min=2, max=144, message=("Name must be between 4 and 100 characters long.")), validators.Regexp(r'^[a-zA-Z0-9 öäå!@#$%^&*(),.?":{}|<>]*$')])
 
     class Meta:
