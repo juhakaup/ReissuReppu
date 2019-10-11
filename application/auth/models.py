1a4eb9289f26291131c6a44c7ffa47296016588a
Juha KauppinenD <juha.kauppinen@helsinki.fi>
2019-10-11 09:35:24 +0300

    Salasanojen hashaaminen

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,18 +1,18 @@
-from application import db
+from application import db, bcrypt
 from application.models import namedCreated
 
 class User(namedCreated):
     __tablename__ = "account"
 
     email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(144), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
     items = db.relationship("Item", backref='item', lazy=True)
     gearlists = db.relationship("GearList", backref='gearlist', lazy=True)
 
     def __init__(self, email, password):
         self.email = email
-        self.password = password
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def get_id(self):
         return self.id
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app, db, login_required
+from application import app, db, login_required, bcrypt
 from application.auth.models import User
 from application.auth.forms import LoginForm, RegisterForm
 
@@ -17,11 +17,12 @@ def auth_login():
     if not form.validate():
         return render_template("auth/loginform.html", form = form)
     
-    # Check if user in db
-    user = User.query.filter_by(email=form.email.data, password=form.password.data).first()
-    if not user:
+    # Check if user in db and password match
+    user = User.query.filter_by(email=form.email.data).first()
+    if not user or not bcrypt.check_password_hash(user.password, form.password.data):
         return render_template("auth/loginform.html", form = form, 
                 error = "No such email or password")
+    
     login_user(user)
     return redirect(url_for("lists_index"))
 
